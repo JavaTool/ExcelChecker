5b4b73dfdd2e769a9f713411d9cc0d5091c9b381
firstfhy <fuhuiyuan@PC-201802281418>
2018-05-02 15:02:37 +0800

    https://github.com/JavaTool/ExcelChecker/issues/2

--- src/main/java/tool/checker/excel/config/ConfigLoader.java
+++ src/main/java/tool/checker/excel/config/ConfigLoader.java
@@ -4,6 +4,26 @@ import tool.checker.excel.data.ExcelsData;
 
 public interface ConfigLoader {
 	
+	String KEY_ALL_PATH = "allPath";
+	
+	String KEY_IGNORE = "ignore";
+	
+	String KEY_PATHS = "paths";
+	
+	String KEY_RELATION_PATH = "relationPath";
+	
+	String KEY_OUT_PATH = "outPath";
+	
+	String VALUE_ALL_PATH = "D:\\workspace\\data\\excel";
+	
+	String VALUE_IGNORE = "";
+	
+	String VALUE_PATHS = "";
+	
+	String VALUE_RELATION_PATH = "ExcelCheckerConfig.xlsx";
+	
+	String VALUE_OUT_PATH = "excel_check_result.txt";
+	
 	void load(ExcelsData excelsData);
 
 }
